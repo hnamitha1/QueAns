dde2168ba31494557059fe4d6853db95dd31c231
hnamitha1 <namitha.h@gmail.com>
2016-05-28 23:34:37 +0100

    adding controoler create method

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,7 @@ Rails.application.routes.draw do
 
   get '/about' => 'home#about'
 
-  post '/questions' => 'home#temp'
-  post '/answers' => 'home#temp'
-
-  get '/questions/:id' => 'home#question'
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
